053b617ee83abf0b17af6c33079b3679b0ed7d3b
Roselene Aparecida da Silva Yokoyama <roselene_31@hotmail.com>
2025-03-24 21:19:08 -0300

    comandos javascript

---
+++ app.js
@@ -0,0 +1,10 @@
+/*alert("Olá seja bem vindo")*/
+console.log("posso escrever aqui também")
+console.log(10+2)
+console.log(10-2)
+console.log(10*2)
+console.log(10/5)
+alert(10+2)
+console.log("10"+2)
+console.log("10"*2)
+console.log(parseFloat("10")+2)
